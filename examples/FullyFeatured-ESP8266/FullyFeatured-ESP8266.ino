a5756376ee0d670d577d7b834e1bbc0a1f3aa976
Lübbe Onken <luebbe.opensource@gmail.com>
2021-02-10 18:47:27 +0100

    Reorder functions to make ESP8266 compile on PlatformIO

--- examples/FullyFeatured-ESP8266/FullyFeatured-ESP8266.ino
+++ examples/FullyFeatured-ESP8266/FullyFeatured-ESP8266.ino
@@ -20,6 +20,11 @@ void connectToWifi() {
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
 void onWifiConnect(const WiFiEventStationModeGotIP& event) {
   Serial.println("Connected to Wi-Fi.");
   connectToMqtt();
@@ -29,11 +34,6 @@ void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
   Serial.println("Disconnected from Wi-Fi.");
   mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
   wifiReconnectTimer.once(2, connectToWifi);
-}
-
-void connectToMqtt() {
-  Serial.println("Connecting to MQTT...");
-  mqttClient.connect();
 }
 
 void onMqttConnect(bool sessionPresent) {
